d640e597739131ba287ea973af438b899b8faead
Trisha <pelonistrisha2002@gmail.com>
2023-12-30 15:22:47 +0800

    modify router

--- js/router/router.js
+++ js/router/router.js
@@ -3,17 +3,8 @@ function setRouter() {
       case "/":
       case "/index.html":
         if (localStorage.getItem("employee_token") !== null) {
-          window.location.pathname = "/index.html";
+          window.location.pathname = "/dashboard.html";
         }
-        break;
-  
-      case "/dashboard.html":
-        if (localStorage.getItem("employee_token") === null) {
-          window.location.pathname = "/index.html";
-        }
-  
-        break;
-      default:
         break;
     }
   }
